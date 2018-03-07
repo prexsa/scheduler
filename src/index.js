94aaf28d248373bd3fbff9932fc0a315983689a8
prexsa <prexgit@gmail.com>
2018-03-07 00:15:16 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './styles/main.scss';
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+};
